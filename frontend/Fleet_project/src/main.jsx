b5bad8d877c461938284ecd3abaf8586841b2e1d
Supremo1717 <ralphmatthewmanabat@gmail.com>
2023-10-10 13:47:13 +0800

    add plugin pwa

--- frontend/Fleet_project/src/main.jsx
+++ frontend/Fleet_project/src/main.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import { registerSW } from "virtual:pwa-register";
 
+// add this to prompt for a refresh
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content available. Reload?")) {
+      updateSW(true);
+    }
+  },
+});
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 )
--- frontend/Fleet_project/vite.config.js
+++ frontend/Fleet_project/vite.config.js
@@ -1,57 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import { VitePWA } from 'vite-plugin-pwa'
-
-// const manifestForPlugIn = {
-//   registerType:'prompt',
-//   includeAssests:['favicon.ico', "apple-touch-icon.png", "maskable_icon.png"],
-//   manifest:{
-//     name:"Karagada",
-//     short_name:"Kargada",
-//     description:"A Freight Management System",
-//     icons:[{
-//       src: '/kargada-logo-192.png',
-//       sizes:'192x192',
-//       type:'image/png',
-      
-//     },
-//     {
-//       src:'/kargada-logo-256.png',
-//       sizes:'256x256',
-//       type:'image/png',
-      
-//     },
-//     {
-//       src: '/kargada-logo-384.png',
-//       sizes:'384x384',
-//       type:'image/png',
-   
-//     },
-//     {
-//       src: '/kargada-logo-512.png',
-//       sizes:'512x512',
-//       type:'image/png',
-   
-//     },
-//     {
-//       src: '/maskable_icon.png',
-//       sizes:'196x196',
-//       type:'image/png',
-//       purpose:'any maskable',
-//     }
-    
-//   ],
-//   theme_color:'#1976d2',
-//   background_color:'#1976d2',
-//   display:"standalone",
-//   scope:'/',
-//   start_url:"/",
-//   orientation:'any'
-//   }
-// }
-// , VitePWA(manifestForPlugIn)
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react()]
+  plugins: [react(), VitePWA({
+    // add this to cache all the imports
+    workbox: {
+      globPatterns: ["**/*"],
+    },
+    // add this to cache all the
+    // static assets in the public folder
+    includeAssets: [
+      "**/*",
+    ]
+  })]
 })
