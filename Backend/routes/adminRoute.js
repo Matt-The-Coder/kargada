0772025b857223a075ef0194754f06c0b168f8fc
Supremo1717 <ralphmatthewmanabat@gmail.com>
2023-10-29 21:14:57 +0800

    added security

--- Backend/routes/adminRoute.js
+++ Backend/routes/adminRoute.js
@@ -1,18 +1,74 @@
 const express = require('express')
 const route = express.Router()
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 const db = require('../database/connection')
-route.get("/Home", (req, res)=>{
-    res.send('hello')
+
+const verifyToken = (req, res, next) => 
+{
+    if(req.cookies.token){
+        req.userToken = req.cookies.token
+        next()
+    }else {
+      return res.json({message:'No token provided.'});
+    }
+
+}
+
+route.get('/homeAuthentication', verifyToken, (req, res) => {
+    jwt.verify(req.userToken, "secretkey", (err, authData)=>{
+        if(err){
+          return res.json({message: "token is expired, not valid!"})
+        }else {
+          return res.json({authData})
+        }
+    })
 })
+
+route.delete("/logout", (req, res) => 
+{
+  res.clearCookie("token")
+  return res.json({message: "Logout Successfully!"})
+})
+
+route.post('/login', async (req, res)=>
+{
+  try { 
+    const {userName, password} = req.body
+    const user = await db(`Select * from accounts where u_username = '${userName}'`)
+    if(user == 0){
+     return res.json({message: "User does not exist"})
+    }
+    else{
+     const checkPassword =  await bcrypt.compare(password, user[0].u_password)
+      if (!checkPassword) {
+          return res.json({message: "Password is Incorrect!"})
+      }
+      else {
+         jwt.sign({user: user}, "secretkey", {expiresIn: '1d'}, (err, token)=>{
+          if(err){
+            return res.json({message: "Cannot create token"})
+          }
+          res.cookie('token', token)
+          return res.json({success:"Login Success!"})
+        })
+      }
+    }
+ 
+  } catch (error) {
+    console.log('You have an error', error)
+  }
+})
+
+
 
 route.get('/register', async (req, res)=>
 {
   try {
     const email = 'ralphmatthewmanabat@gmail.com'
-    const username = 'superAdmin'
-    const name = 'superAdmin'
-    const password = 'mattTheCoder#17'
+    const username = 'rm'
+    const name = 'rm'
+    const password = '123'
     const hashedPassword = await bcrypt.hash(password, 10)
     const sqlQuery = `INSERT INTO accounts (u_name, u_username, u_password, u_email) VALUES('${name}', 
     '${username}', '${hashedPassword}', '${email}')`;
@@ -24,27 +80,4 @@ route.get('/register', async (req, res)=>
 
 
 })
-
-route.post('/login', async (req, res)=>
-{
-  try { 
-    const {userName, password} = req.body
-    const result = await db(`Select * from accounts where u_username = '${userName}'`)
-    if(result == 0){
-     return res.json({message: "User does not exist"})
-    }
-    else{
-     const checkPassword =  await bcrypt.compare(password, result[0].u_password)
-      if (!checkPassword) {
-          return res.json({message: "Password is Incorrect!"})
-      }
-      else return res.json({message: "Successfully Logged In!"})
-    }
- 
-  } catch (error) {
-    console.log('You have an error', error)
-  }
-
-})
-
 module.exports = route
--- Backend/server.js
+++ Backend/server.js
@@ -3,15 +3,18 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT
 const cors = require('cors') 
+const cookieParser = require('cookie-parser')
 app.use(express.json());
-
+app.use(cookieParser())
 var corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: ["http://127.0.0.1:5173"],
+    methods: ["POST", "GET", "DELETE"],
+    credentials: true,
     optionsSuccessStatus: 200 
   }
 
 const routes = require('./routes/adminRoute')
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(routes)
 app.listen(port, ()=>{
 
