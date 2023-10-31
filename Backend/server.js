b5b0de0f82d9bc9cd7d997dd5e3e71eb7b7a8359
Supremo1717 <ralphmatthewmanabat@gmail.com>
2023-10-31 18:44:03 +0800

    add env

--- Backend/server.js
+++ Backend/server.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const express = require('express')
 const app = express()
 const port = process.env.PORT
+const origin = process.env.ORIGIN
 const cors = require('cors') 
 const cookieParser = require('cookie-parser')
 app.use(express.json());
 app.use(cookieParser())
 var corsOptions = {
-    origin: ["http://127.0.0.1:5173"],
+    origin: [origin],
     methods: ["POST", "GET", "DELETE"],
     credentials: true,
     optionsSuccessStatus: 200 
