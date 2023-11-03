398291ab9e3930261c3be7a0ddb549585884244e
Supremo1717 <ralphmatthewmanabat@gmail.com>
2023-11-03 11:54:01 +0800

    remove pwa

--- frontend/Fleet_project/vite.config.js
+++ frontend/Fleet_project/vite.config.js
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+// import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react(), VitePWA({
-    // add this to cache all the imports
-    workbox: {
-      globPatterns: ["**/*"],
-    },
-    // add this to cache all the
-    // static assets in the public folder
-    includeAssets: [
-      "**/*",
-    ]
-  })]
+  plugins: [react()]
 })
+// VitePWA({
+//   // add this to cache all the imports
+//   workbox: {
+//     globPatterns: ["**/*"],
+//   },
+//   // add this to cache all the
+//   // static assets in the public folder
+//   includeAssets: [
+//     "**/*",
+//   ]
+// })
